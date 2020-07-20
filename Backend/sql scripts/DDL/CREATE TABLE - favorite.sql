ba8ec55a35d777abebbc490a0820ad580862f4c8
yiftachsa <yiftachs@post.bgu.ac.il>
2020-07-20 17:41:44 +0300

    .gitignore is now working

--- Backend/sql scripts/DDL/CREATE TABLE - favorite.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE dbo.favorite
-CREATE TABLE [favorite] (
-	[author] [UNIQUEIDENTIFIER] NOT NULL,
-	[recipe_id] varchar(255) NOT NULL,
-	PRIMARY KEY ([author], [recipe_id]),
-    FOREIGN KEY ([author]) REFERENCES users([user_id])
-)
--- Frontend/src/pages/NotFoundPage.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container">
-    <h1>Four Oh Four you didn't</h1>
-    <router-link to="/" exact>ET Go Home</router-link>
-  </div>
-</template>
